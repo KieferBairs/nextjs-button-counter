ef45f5ac77fdac37ac9860f50731576b5c256310
Kiefer Bairstow <bairstow@usf.edu>
2025-10-23 21:17:34 -0400

    Implemented aria-live polite to allow accesibility

--- components.tsx
+++ components.tsx
@@ -14,7 +14,26 @@ export default function Counter({ initialCount = 0, intitialStep = 1 }: CounterP
     const [count, setCount] = useState<number>(initialCount);
     const [step, setStep] = useState<number>(Math,max(1, intitialStep);
 
-    // useId created a unique ID label for accessibility
-    const stepId = useId();
+// useId created a unique ID label for accessibility
+const stepId = useId();
 
-    
+// Only allow decrrementing if count-step will not go below 0
+const canDecrement = count - step >= 0;
+
+return (
+    <section aria-label="Button counter">
+        <div>
+            <label htmlFor={stepId}>Step</label>
+            <input
+                id={stepId}
+                type="number"
+                min={1} // Dont allow step to be less than 1
+                value={step}
+                onChange={(e) => setStep(Math.max(1, Number(e.target.value || 1)))}
+              />        
+            </div>
+
+{/* aria-live="polite" lets screen readers know to announce changes */}
+<p aria-live="polite" aria-atomic="true">
+    count: <strong>{count}</strong>
+</p>
